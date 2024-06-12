dd283cb26813f730a1a9336ea6c7c83b202dfa0d
NikitaTerekhin <nterekhin@hiypcorp.com>
2024-06-12 15:31:41 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const tg = window.Telegram.WebApp;
+import {ref, onMounted} from 'vue'
+
+defineProps<{ msg: string }>()
+
+const isReady = ref(false);
+const close = () => {
+    tg.close();
+}
+
+onMounted(async () => {
+    await tg.ready();
+    isReady.value = true;
+})
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <p v-if="isReady">ready</p>
+    <button @click="close">close</button>
+</template>
